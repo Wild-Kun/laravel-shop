d79604462b7d20f33e000f3b0eb4268a03c106b7
王坤 <1941483751@qq.com>
2020-07-16 13:49:11 +0800

    用户界面 - 商品筛选、排序

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -7,8 +7,37 @@ use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
-    public function index(Request $request){
-        $products=Product::query()->where('on_sale',true)->paginate(16);
-        return view('products.index',['products'=>$products]);
+    public function index(Request $request)
+    {
+        //  创建查询构造器
+        $builder = Product::query()->where('on_sale', true);
+
+        if ($search = $request->input('search', '')) {
+            $like = '%' . $search . '%';
+            $builder->where(function ($query) use ($like) {
+                $query->where('title', 'like', $like);
+                $query->orWhere('description', 'like', $like);
+                $query->orWhereHas('skus', function ($query) use ($like) {
+                    $query->where('title', 'like', $like);
+                    $query->orWhere('description', 'like', $like);
+                });
+            });
+        }
+        // 是否有提交 order 参数，如果有就赋值给 $order 变量
+        // order 参数用来控制商品的排序规则
+        if ($order = $request->input('order', '')) {
+            if (preg_match('/^(.+)_(asc|desc)$/', $order, $m)) {
+                if (in_array($m[1], ['price', 'sold_count', 'rating'])) {
+                    $builder->orderBy($m[1], $m[2]);
+                }
+            }
+        }
+        $products = $builder->paginate(16);
+
+        return view('products.index', [
+            'products' => $products, 'filters' => [
+                'search' => $search, 'order' => $order,
+            ],
+        ]);
     }
 }
