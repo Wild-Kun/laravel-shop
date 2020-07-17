0e81b9d7d6d0085f7a0bf7e7ef2eff0f80ee7e92
王坤 <1941483751@qq.com>
2020-07-17 17:19:02 +0800

    引入支付库

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Monolog\Logger;
+use Yansongda\Pay\Pay;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,7 +16,28 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton('alipay',function(){
+            $config=config('pay.alipay');
+            if (app()->environment() !=='production'){
+                $config['mode']='dev';
+                $config['log']['level']=Logger::DEBUG;
+            }else{
+                $config['log']['level']=Logger::WARNING;
+            }
+            // 创建支付宝支付对象
+            return Pay::alipay($config);
+        });
+
+        $this->app->singleton('wechat_pay',function(){
+            $config=config('pay.wechat');
+            if (app()->environment() !=='production'){
+                $config['log']['level']=Logger::DEBUG;
+            }else{
+                $config['log']['level']=Logger::WARNING;
+            }
+            // 创建微信支付对象
+            return Pay::wechat($config);
+        });
     }
 
     /**
