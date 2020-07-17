fc80a5ec8f2e57d7413883ba182d9c16d9fe82cf
王坤 <1941483751@qq.com>
2020-07-17 17:03:39 +0800

    封装业务代码

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -2,70 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use App\Exceptions\InvalidRequestException;
 use App\Http\Requests\OrderRequest;
+use App\Services\OrderService;
 use Illuminate\Http\Request;
-use App\Jobs\CloseOrder;
 use App\Models\Order;
-use App\Models\ProductSku;
 use App\Models\UserAddress;
-use Illuminate\Support\Carbon;
 
 class OrdersController extends Controller
 {
-    public function store(OrderRequest $request)
+    public function store(OrderRequest $request,OrderService $orderService)
     {
 
         $user = $request->user();
+        $address = UserAddress::find($request->input('address_id'));
 
-        $order = \DB::transaction(function () use ($user, $request) {
-            $address = UserAddress::find($request->input('address_id'));
-            $address->update(['last_used_at' => Carbon::now()]);
-
-            $order = new Order([
-                'address' => [
-                    'address' => $address->full_address, 'zip' => $address->zip,
-                    'contact_name' => $address->contact_name, 'contact_phone' => $address->contact_phone,
-                ], 'remark' => $request->input('remark'), 'total_amount' => 0
-            ]);
-
-            // 订单关联当前用户
-            $order->user()->associate($user);
-
-            $order->save();
-
-            $totalAmount = 0;
-            $items = $request->input('items');
-            // 然后遍历传入的商品 SKU 及其数量，$order->items()->make() 方法可以新建一个关联关系的对象
-            //（也就是 OrderItem）但不保存到数据库，这个方法等同于 $item = new OrderItem();
-            foreach ($items as $data) {
-                $sku = ProductSku::find($data['sku_id']);
-                $item = $order->items()->make([
-                    'amount' => $data['amount'], 'price' => $sku->price,
-                ]);
-                $item->product()->associate($sku->product_id);
-                $item->productSku()->associate($sku);
-                $item->save();
-                $totalAmount += $sku->price * $data['amount'];
-                if ($sku->decreaseStock($data['amount']) <= 0) {
-                    throw new InvalidRequestException('该商品库存不足');
-                }
-            }
-
-            // 更新订单总金额
-            $order->update(['total_amount' => $totalAmount]);
-
-            // 将下单的商品从购物车中删除
-            $skuIds = collect($items)->pluck('sku_id');
-            dump($skuIds);
-            $user->cartItems()->whereIn('product_sku_id', $skuIds)->delete();
-
-            return $order;
-        });
-
-        $this->dispatch(new CloseOrder($order, config('app.order_ttl')));
-
-        return $order;
+        return $orderService->store($user, $address, $request->input('remark'), $request->input('items'));
     }
 
     public function index(Request $request)
